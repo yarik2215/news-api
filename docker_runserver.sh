df1776cb7ae5c345ac40aafb1c74c54a14bd516c
Yaroslav <stikblacklabel@gmail.com>
2021-01-21 23:18:58 +0200

    fix run script again

--- docker_runserver.sh
+++ docker_runserver.sh
@@ -4,5 +4,5 @@ echo "Run manage.py migrate"
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 echo "Run server"
-PORT="${VARIABLE:=8000}"
+PORT="${PORT:=8000}"
 exec ./manage.py rqworker --with-schedule & gunicorn news.wsgi -b 0.0.0.0:$PORT
